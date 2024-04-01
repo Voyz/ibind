fafc473568de39b88997231002adae7a87c4e803
voyz <voy1982@yahoo.co.uk>
2024-04-01 19:19:19 +0700

    updated __init__ definitions

--- ibind/__init__.py
+++ ibind/__init__.py
@@ -1,3 +1,5 @@
 import support.logs as logs
-from client.ibkr_client import IbkrClient as IbkrClient
-from client.ibkr_ws_client import IbkrWsClient as IbkrWsClient
+from client.ibkr_client import IbkrClient
+from client.ibkr_ws_client import IbkrWsClient
+from client.ibkr_ws_client import IbkrWsKey
+from client.ibkr_ws_client import IbkrSubscriptionProcessor
