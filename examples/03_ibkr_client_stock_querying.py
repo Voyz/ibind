e09fd7177f7089415aa034533ea2df027a7ea2e9
voyz <voy1982@yahoo.co.uk>
2024-04-01 19:19:51 +0700

    updated ibkr_client examples

---
+++ examples/03_ibkr_client_stock_querying.py
@@ -0,0 +1,50 @@
+from pprint import pprint
+
+import ibind
+import var
+from client.ibkr_utils import StockQuery
+from ibind import IbkrClient
+
+ibind.logs.initialize(log_to_file=False)
+
+c = IbkrClient(
+    url='https://localhost:5000/v1/api/',
+    cacert=var.IBKR_CACERT,
+)
+
+
+print('#### get_stocks ####')
+stocks = c.get_stocks('AAPL').data
+print(stocks)
+
+
+print('\n#### get_conids ####')
+conids = c.get_conids('AAPL').data
+print(conids)
+
+
+print('\n#### using StockQuery ####')
+conids = c.get_conids(StockQuery('AAPL', contract_conditions={'exchange': 'MEXI'}), default_filtering=False).data
+pprint(conids)
+
+
+print('\n#### mixed queries ####')
+stock_queries = [
+    StockQuery('AAPL', contract_conditions={'exchange': 'MEXI'}),
+    'HUBS',
+    StockQuery('GOOG', name_match='ALPHABET INC - CDR')
+]
+conids = c.get_conids(stock_queries, default_filtering=False).data
+pprint(conids)
+
+
+"""
+    The get_conids() method will raise an exception if the filtered stocks response doesn't provide exactly one conid.
+    The default_filtering filtered the returned contracts by isUS=True which usually returns only one conid.
+    If multiple conids are found, you must provide additional conditions for the particular stock in order in order to ensure only one conid is returned. 
+    
+    Uncomment the following lines to see the exception raised when multiple conids are returned.
+"""
+# print('\n#### get_conid with too many conids ####')
+# conids = c.get_conids('AAPL', default_filtering=False).data
+# pprint(conids)
---
+++ examples/04_ibkr_client_submit_order.py
@@ -0,0 +1,56 @@
+import datetime
+import os
+from unittest.mock import patch, MagicMock
+
+import ibind
+import var
+from client.ibkr_utils import make_order_request, QuestionType
+from ibind import IbkrClient
+
+ibind.logs.initialize(log_to_file=False)
+
+account_id = os.getenv('IBKR_ACCOUNT_ID', '[YOUR_ACCOUNT_ID]')
+c = IbkrClient(
+    url='https://localhost:5000/v1/api/',
+    account_id=account_id,
+    cacert=var.IBKR_CACERT,
+)
+
+conid = '265598'
+side = 'BUY'
+size = 1
+order_type = 'MARKET'
+price = 100
+order_tag = f'my_order-{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}'
+
+order_request = make_order_request(
+    conid=conid,
+    side=str(side),
+    quantity=float(size),
+    order_type=order_type,
+    price=float(price),
+    acct_id=account_id,
+    coid=order_tag
+)
+
+answers = {
+    QuestionType.PRICE_PERCENTAGE_CONSTRAINT: True,
+    QuestionType.ORDER_VALUE_LIMIT: True
+}
+
+mocked_responses = [
+    [{'id': 0, 'message': ['price exceeds the Percentage constraint of 3%.']}],
+    [{'id': 1, 'message': ['exceeds the Total Value Limit of']}],
+    [{'success': True}]
+]
+
+print('#### submit_order ####')
+
+# We mock the requests module to prevent submitting orders in this example script.
+# Comment out the next two lines if you'd like to actually submit the orders to IBKR.
+with patch('base.rest_client.requests') as requests_mock:
+    requests_mock.request.return_value = MagicMock(json=MagicMock(side_effect=mocked_responses))
+
+    response = c.submit_order(order_request, answers, account_id).data
+
+print(response)
