d085b9b0b2a9dfcb517e701244aa04e6e9a7e3e6
voyz <voy1982@yahoo.co.uk>
2024-04-03 18:59:18 +0700

    updated 05_ibkr_client_marketdata_history.py with more examples

--- examples/05_ibkr_client_marketdata_history.py
+++ examples/05_ibkr_client_marketdata_history.py
@@ -15,25 +15,41 @@ c = IbkrClient(
 )
 
 st = time.time()
+history = c.marketdata_history_by_conid('265598', period='1min', bar='1min', outside_rth=True)
+diff_one_conid = time.time() - st
+print('#### One conid ####')
+print(f'{history}')
+
+
+st = time.time()
+history = c.marketdata_history_by_symbol('AAPL', period='1min', bar='1min', outside_rth=True)
+diff_one_symbol_raw = time.time() - st
+print('\n\n#### One symbol raw ####')
+print(f'{history}')
+
+
+st = time.time()
 history = c.marketdata_history_by_symbols('AAPL', period='1min', bar='1min', outside_rth=True)
-diff1 = time.time() - st
-print('#### One symbol ####')
+diff_one_symbol = time.time() - st
+print('\n\n#### One symbol ####')
 print(f'{history}')
 
 
 st = time.time()
 history = c.marketdata_history_by_symbols(['AAPL', 'MSFT', 'GOOG', 'TSLA', 'AMZN'], period='1min', bar='1min', outside_rth=True)
-diff2 = time.time() - st
+diff_five_symbols = time.time() - st
 print('\n\n#### Five symbols ####')
 print(f'{history}')
 
 
 st = time.time()
 history = c.marketdata_history_by_symbols(['AAPL', 'MSFT', 'GOOG', 'TSLA', 'AMZN', 'ADBE', 'AMD', 'COIN', 'META', 'DIS', 'BAC', 'XOM', 'KO', 'WMT', 'V'], period='1min', bar='1min', outside_rth=True)
-diff3 = time.time() - st
+diff_fifteen_symbols = time.time() - st
 print('\n\n#### Fifteen symbols ####')
 print(f'{history}')
 
-print(f'\n\n1 symbol took: {diff1:.2f}s')
-print(f'5 symbols took: {diff2:.2f}s')
-print(f'15 symbols took: {diff3:.2f}s')
+print(f'\n\n1 conid took: {diff_one_conid:.2f}s')
+print(f'1 symbol raw took: {diff_one_symbol_raw:.2f}s')
+print(f'1 symbol took: {diff_one_symbol:.2f}s')
+print(f'5 symbols took: {diff_five_symbols:.2f}s')
+print(f'15 symbols took: {diff_fifteen_symbols:.2f}s')
