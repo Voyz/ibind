338c64dfadf42eb890546d858738293c84cc22c5
voyz <voy1982@yahoo.co.uk>
2024-04-05 09:39:40 +0700

    added more classes to public API

--- ibind/__init__.py
+++ ibind/__init__.py
@@ -1,10 +1,12 @@
-from ibind.support import logs
+from ibind.base.queue_controller import QueueAccessor
+from ibind.base.rest_client import Result
 from ibind.client.ibkr_client import IbkrClient
 from ibind.client.ibkr_ws_client import IbkrWsClient
 from ibind.client.ibkr_ws_client import IbkrWsKey
 from ibind.client.ibkr_ws_client import IbkrSubscriptionProcessor
 from ibind.client.ibkr_utils import StockQuery, make_order_request, QuestionType
 from ibind.client.ibkr_definitions import snapshot_keys_to_ids
+from ibind.support import logs
 
 ibind_logs_initialize = logs.initialize
 
@@ -18,6 +20,8 @@ __all__ = [
     'make_order_request',
     'QuestionType',
     'snapshot_keys_to_ids',
+    'Result',
+    'QueueAccessor'
 ]
 
 __version__ = "0.0.1"
