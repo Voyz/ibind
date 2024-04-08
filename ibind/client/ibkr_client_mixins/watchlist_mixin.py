531552d10fc6fce5d102db9622a309f8c8facf2a
voyz <voy1982@yahoo.co.uk>
2024-04-08 23:43:37 +0700

    added watchlist_mixin.py

---
+++ ibind/client/ibkr_client_mixins/watchlist_mixin.py
@@ -0,0 +1,25 @@
+from typing import TYPE_CHECKING, Union, Dict, List
+
+from ibind.support.logs import project_logger
+
+if TYPE_CHECKING:  # pragma: no cover
+    from ibind import IbkrClient, Result
+
+_LOGGER = project_logger(__file__)
+
+
+class WatchlistMixin():
+
+    def create_watchlist(
+            self: 'IbkrClient',
+            id: str,
+            name: str,
+            rows: List[Dict[str, Union[str, int]]]
+    ) -> Result:  # pragma: no cover
+        return self.post('iserver/watchlist', params={'id':id, 'name': name, 'rows':rows})
+
+    def get_all_watchlists(
+            self: 'IbkrClient',
+            sc:str = 'USER_WATCHLIST'
+    ) -> Result:  # pragma: no cover
+        return self.get('iserver/watchlist', params={'SC':sc})
