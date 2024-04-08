cfa271824dde2753c4bad2d9b87035311f217b9c
voyz <voy1982@yahoo.co.uk>
2024-04-08 23:45:14 +0700

    updated marketdata_mixin with new endpoints

--- ibind/client/ibkr_client_mixins/marketdata_mixin.py
+++ ibind/client/ibkr_client_mixins/marketdata_mixin.py
@@ -6,27 +6,90 @@ from ibind.client.ibkr_definitions import decode_data_availability
 from ibind.client.ibkr_utils import StockQuery, StockQueries
 from ibind.support.errors import ExternalBrokerError
 from ibind.support.logs import project_logger
-from ibind.support.py_utils import ensure_list_arg, OneOrMany, execute_in_parallel
+from ibind.support.py_utils import ensure_list_arg, OneOrMany, execute_in_parallel, params_dict
 
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover
     from ibind import IbkrClient
 
 _LOGGER = project_logger(__file__)
 
+
 class MarketdataMixin():
+
+    @ensure_list_arg('conids')
+    def live_marketdata_snapshot(self: 'IbkrClient', conids: OneOrMany[str], fields: OneOrMany[str]) -> Result:
+        params = {
+            'conids': ','.join(conids),
+            'fields': ','.join(fields)
+        }
+        return self.get(f'iserver/secdef/search', params)
+
+    def regulatory_snapshot(self: 'IbkrClient', conid: str) -> Result:
+        return self.get(f'md/regsnapshot', {'conid': conid})
+
     def marketdata_history_by_conid(
             self: 'IbkrClient',
             conid: str,
+            bar: str,
             exchange: str = None,
             period: str = None,
-            bar: str = None,
-            outside_rth: bool = None
+            outside_rth: bool = None,
+            start_time: datetime.datetime = None
     ) -> Result:  # pragma: no cover
         """
         period: {1-30}min, {1-8}h, {1-1000}d, {1-792}w, {1-182}m, {1-15}y
         bar: 1min, 2min, 3min, 5min, 10min, 15min, 30min, 1h, 2h, 3h, 4h, 8h, 1d, 1w, 1m
         """
-        return self.get('iserver/marketdata/history', params={"conid": int(conid), "exchange": exchange, "period": period, "bar": bar, "outsideRth": outside_rth})
+        params = params_dict(
+            {
+                'conid': conid,
+                'bar': bar
+            },
+            optional={
+                'exchange': exchange,
+                'period': period,
+                'outsideRth': outside_rth,
+                'startTime': start_time
+            },
+            preprocessors={
+                'startTime': lambda x: x.strftime('')
+            }
+        )
+
+        return self.get('iserver/marketdata/history', params)
+
+    def historical_marketdata_beta(
+            self: 'IbkrClient',
+            conid: str,
+            period: str,
+            bar: str,
+            outside_rth: bool = None,
+            start_time: datetime.datetime = None,
+            direction: str = None,
+            bar_type: str = None,
+    ) -> Result:  # pragma: no cover
+        """
+        period: {1-30}min, {1-8}h, {1-1000}d, {1-792}w, {1-182}m, {1-15}y
+        bar: 1min, 2min, 3min, 5min, 10min, 15min, 30min, 1h, 2h, 3h, 4h, 8h, 1d, 1w, 1m
+        """
+        params = params_dict(
+            {
+                'conid': conid,
+                'period': period,
+                'bar': bar
+            },
+            optional={
+                'outsideRth': outside_rth,
+                'startTime': start_time,
+                'direction': direction,
+                'barType': bar_type,
+            },
+            preprocessors={
+                'startTime': lambda x: x.strftime('')
+            }
+        )
+
+        return self.get('hmds/history', params)
 
     def marketdata_history_by_symbol(
             self: 'IbkrClient',
@@ -39,12 +102,11 @@ class MarketdataMixin():
         conid = self.get_conids(symbol).data[symbol]
         return self.marketdata_history_by_conid(conid, exchange, period, bar, outside_rth)
 
-
     @ensure_list_arg('conids')
     def marketdata_unsubscribe(self: 'IbkrClient', conids: OneOrMany[int]):
         # we unsubscribe from all conids simultaneously
         unsubscribe_requests = {conid: {'args': [f'iserver/marketdata/{conid}/unsubscribe']} for conid in conids}
-        results = execute_in_parallel(self.get, unsubscribe_requests)
+        results = execute_in_parallel(self.post, unsubscribe_requests)
 
         for conid, result in results.items():
             if isinstance(result, Exception):
@@ -55,10 +117,8 @@ class MarketdataMixin():
 
         return results
 
-
     def marketdata_unsubscribe_all(self: 'IbkrClient') -> Result:  # pragma: no cover
         return self.get(f'iserver/marketdata/unsubscribeall')
-
 
     @ensure_list_arg('queries')
     def marketdata_history_by_symbols(
