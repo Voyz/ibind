e6b258536def181453e39a67f95d1f735930cba5
voyz <voy1982@yahoo.co.uk>
2024-04-24 17:43:51 +0800

    added inline docs for watchlist_mixin.py and added 'get_watchlist_information' and 'delete_watchlist' endpoints support

--- ibind/client/ibkr_client_mixins/watchlist_mixin.py
+++ ibind/client/ibkr_client_mixins/watchlist_mixin.py
@@ -1,7 +1,7 @@
 from typing import TYPE_CHECKING, Union, Dict, List
 
-from ibind.support.logs import project_logger
 from ibind.base.rest_client import Result
+from ibind.support.logs import project_logger
 
 if TYPE_CHECKING:  # pragma: no cover
     from ibind import IbkrClient
@@ -17,10 +17,41 @@ class WatchlistMixin():
             name: str,
             rows: List[Dict[str, Union[str, int]]]
     ) -> Result:  # pragma: no cover
-        return self.post('iserver/watchlist', params={'id':id, 'name': name, 'rows':rows})
+        """
+        Create a watchlist to monitor a series of contracts.
 
-    def get_all_watchlists(
-            self: 'IbkrClient',
-            sc:str = 'USER_WATCHLIST'
-    ) -> Result:  # pragma: no cover
-        return self.get('iserver/watchlist', params={'SC':sc})
+        Parameters:
+            id (str): Supply a unique identifier to track a given watchlist. Must supply a number.
+            name (str): Supply the human readable name of a given watchlist. Displayed in TWS and Client Portal.
+            rows (List[Dict[str, Union[str, int]]]): Provide details for each contract or blank space in the watchlist. Each object may include:
+                - C (int): Provide the conid, or contract identifier, of the conid to add.
+                - H (str): Can be used to add a blank row between contracts in the watchlist.
+        """
+        return self.post('iserver/watchlist', params={'id': id, 'name': name, 'rows': rows})
+
+    def get_all_watchlists(self: 'IbkrClient', sc: str = 'USER_WATCHLIST') -> Result:  # pragma: no cover
+        """
+        Retrieve a list of all available watchlists for the account.
+
+        Parameters:
+            SC (str): Optional. Specify the scope of the request. Valid Values: USER_WATCHLIST.
+        """
+        return self.get('iserver/watchlist', params={'SC': sc})
+
+    def get_watchlist_information(self: 'IbkrClient', id: str) -> Result:  # pragma: no cover
+        """
+        Request the contracts listed in a particular watchlist.
+
+        Parameters:
+            id (str): Set equal to the watchlist ID you would like data for.
+        """
+        return self.get('iserver/watchlist', params={'id': id})
+
+    def delete_watchlist(self: 'IbkrClient', id: str) -> Result:  # pragma: no cover
+        """
+        Permanently delete a specific watchlist for all platforms.
+
+        Parameters:
+            id (str): Include the watchlist ID you wish to delete.
+        """
+        return self.delete('iserver/watchlist', params={'id': id})
