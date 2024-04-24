5155f72a9f6230d27fc04a865a9e8afa3a08a6c7
voyz <voy1982@yahoo.co.uk>
2024-04-24 17:43:22 +0800

    added inline docs for scanner_mixin.py and session_mixin.py

--- ibind/client/ibkr_client_mixins/scanner_mixin.py
+++ ibind/client/ibkr_client_mixins/scanner_mixin.py
@@ -12,6 +12,9 @@ _LOGGER = project_logger(__file__)
 
 class ScannerMixin():
     def scanner_parameters(self: 'IbkrClient') -> Result:
+        """
+        Returns an xml file containing all available parameters to be sent for the Iserver scanner request.
+        """
         return self.get('iserver/scanner/params')
 
     def market_scanner(
@@ -21,6 +24,18 @@ class ScannerMixin():
             location: str,
             filter: List[Dict[str, str]] = None
     ) -> Result:
+        """
+        Searches for contracts according to the filters specified in /iserver/scanner/params endpoint.
+        Users can receive a maximum of 50 contracts from 1 request.
+
+        Parameters:
+            instrument (str): Instrument type as the target of the market scanner request. Found in the “instrument_list” section of the /iserver/scanner/params response.
+            type (str): Scanner value the market scanner is sorted by. Based on the “scan_type_list” section of the /iserver/scanner/params response.
+            location (str): Location value the market scanner is searching through. Based on the “location_tree” section of the /iserver/scanner/params response.
+            filter (List[Dict[str, str]]): Contains any additional filters that should apply to response. Each filter object may include:
+                - code (str): Code value of the filter. Based on the “code” value within the “filter_list” section of the /iserver/scanner/params response.
+                - value (int): Value corresponding to the input for “code”.
+        """
         params = params_dict(
             {
                 'instrument': instrument,
@@ -31,6 +46,9 @@ class ScannerMixin():
         return self.post('iserver/scanner/run', params)
 
     def hmds_scanner_parameters(self: 'IbkrClient') -> Result:
+        """
+        Query the parameter list for the HMDS market scanner.
+        """
         return self.get('hmds/scanner/params')
 
     def hmds_market_scanner(
@@ -39,9 +57,21 @@ class ScannerMixin():
             location: str,
             scan_code: str,
             sec_type: str,
+            filter: List[Dict[str, str]],
             max_items: int = None,
-            filter: List[Dict[str, str]] = None
     ) -> Result:
+        """
+        Request a market scanner from our HMDS service.
+        Can return a maximum of 250 contracts.
+
+        Parameters:
+            instrument (str): Specify the type of instrument for the request. Found under the “instrument_list” value of the /hmds/scanner/params request.
+            locations (str): Specify the type of location for the request. Found under the “location_tree” value of the /hmds/scanner/params request.
+            scanCode (str): Specify the scanner type for the request. Found under the “scan_type_list” value of the /hmds/scanner/params request.
+            secType (str): Specify the type of security type for the request. Found under the “location_tree” value of the /hmds/scanner/params request.
+            filters (List[Dict[str, str]]): Array of objects containing all filters upon the scanner request. While “filters” must be specified in the body, no content in the array needs to be passed.
+            maxItems (int, optional): Specify how many items should be returned. Default and maximum set to 250.
+        """
         params = params_dict(
             {
                 'instrument': instrument,
--- ibind/client/ibkr_client_mixins/session_mixin.py
+++ ibind/client/ibkr_client_mixins/session_mixin.py
@@ -14,21 +14,45 @@ _LOGGER = project_logger(__file__)
 
 class SessionMixin():
     def authentication_status(self: 'IbkrClient') -> Result:  # pragma: no cover
+        """
+        Current Authentication status to the Brokerage system. Market Data and Trading is not possible if not authenticated, e.g. authenticated shows false.
+        """
         return self.post('iserver/auth/status')
 
     def initialize_brokerage_session(self: 'IbkrClient', publish: bool, compete: bool) -> Result:  # pragma: no cover
+        """
+        After retrieving the access token and subsequent Live Session Token, customers can initialize their brokerage session with the ssodh/init endpoint.
+        NOTE: This is essential for using all /iserver endpoints, including access to trading and market data.
+
+        Parameters:
+            publish (Boolean): Determines if the request should be sent immediately. Users should always pass true. Otherwise, a ‘500’ response will be returned.
+            compete (Boolean): Determines if other brokerage sessions should be disconnected to prioritize this connection.
+        """
         return self.post('iserver/auth/ssodh/init', {'publish': publish, 'compete': compete})
 
     def logout(self: 'IbkrClient') -> Result:  # pragma: no cover
+        """
+        Logs the user out of the gateway session. Any further activity requires re-authentication.
+        """
         return self.post('logout')
 
     def tickle(self: 'IbkrClient') -> Result:  # pragma: no cover
+        """
+        If the gateway has not received any requests for several minutes an open session will automatically timeout. The tickle endpoint pings the server to prevent the session from ending. It is expected to call this endpoint approximately every 60 seconds to maintain the connection to the brokerage session.
+        """
         return self.post('tickle', log=False)
 
     def reauthenticate(self: 'IbkrClient') -> Result:  # pragma: no cover
+        """
+        When using the CP Gateway, this endpoint provides a way to reauthenticate to the Brokerage system as long as there is a valid brokerage session.
+        All interest in reauthenticating the gateway session should be handled using the /iserver/auth/ssodh/init endpoint.
+        """
         return self.post('iserver/reauthenticate')
 
     def validate(self: 'IbkrClient') -> Result:  # pragma: no cover
+        """
+        Validates the current session for the SSO user.
+        """
         return self.get('/sso/validate')
 
     def check_health(self: 'IbkrClient'):
