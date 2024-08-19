be1b56733800c55f9fb7d6b2b4a9804a772c428d
voyz <voy1982@yahoo.co.uk>
2024-08-19 17:14:59 +0200

    replaced deprecated strtobool

--- ibind/var.py
+++ ibind/var.py
@@ -1,7 +1,27 @@
 import os
 import tempfile
 
-from distutils.util import strtobool
+# from: https://github.com/drgarcia1986/simple-settings/pull/281/files
+_MAP = {
+    'y': True,
+    'yes': True,
+    't': True,
+    'true': True,
+    'on': True,
+    '1': True,
+    'n': False,
+    'no': False,
+    'f': False,
+    'false': False,
+    'off': False,
+    '0': False
+}
+
+def strtobool(value):
+    try:
+        return _MAP[str(value).lower()]
+    except KeyError:
+        raise ValueError('"{}" is not a valid bool value'.format(value))
 
 
 def to_bool(value):
