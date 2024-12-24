83573bf88c803f68ca33045d2e793cb4a0e39a95
voyz <voy1982@yahoo.co.uk>
2024-12-24 09:42:08 +0100

    added oauth_init and oauth_shutdown to IbkrClient

--- ibind/client/ibkr_client.py
+++ ibind/client/ibkr_client.py
@@ -11,8 +11,8 @@ from ibind.client.ibkr_client_mixins.portfolio_mixin import PortfolioMixin
 from ibind.client.ibkr_client_mixins.scanner_mixin import ScannerMixin
 from ibind.client.ibkr_client_mixins.session_mixin import SessionMixin
 from ibind.client.ibkr_client_mixins.watchlist_mixin import WatchlistMixin
+from ibind.client.ibkr_utils import Tickler
 from ibind.support.logs import new_daily_rotating_file_handler, project_logger
-from ibind.support.oauth import req_live_session_token, generate_oauth_headers
 
 _LOGGER = project_logger(__file__)
 
@@ -36,7 +36,6 @@ class IbkrClient(RestClient, AccountsMixin, ContractMixin, MarketdataMixin, Orde
 
     def __init__(
             self,
-            use_oauth: bool = False,
             account_id: Optional[str] = var.IBIND_ACCOUNT_ID,
             url: str = var.IBIND_REST_URL,
             host: str = 'localhost',
@@ -45,6 +44,7 @@ class IbkrClient(RestClient, AccountsMixin, ContractMixin, MarketdataMixin, Orde
             cacert: Union[str, os.PathLike, bool] = var.IBIND_CACERT,
             timeout: float = 10,
             max_retries: int = 3,
+            use_oauth: bool = False,
     ) -> None:
         """
         Parameters:
@@ -57,6 +57,7 @@ class IbkrClient(RestClient, AccountsMixin, ContractMixin, MarketdataMixin, Orde
                                                          or False to disable SSL verification. Defaults to False.
             timeout (float, optional): Timeout in seconds for the API requests. Defaults to 10.
             max_retries (int, optional): Maximum number of retries for failed API requests. Defaults to 3.
+            use_oauth (bool, optional): Whether to use OAuth authentication. Defaults to False.
         """
 
         self._use_oauth = use_oauth
@@ -73,20 +74,50 @@ class IbkrClient(RestClient, AccountsMixin, ContractMixin, MarketdataMixin, Orde
         self.logger.info(f'New IbkrClient(base_url={self.base_url!r}, account_id={self.account_id!r}, ssl={self.cacert!r}, timeout={self._timeout}, max_retries={self._max_retries})')
 
         if self._use_oauth:
-            self.live_session_token, self.live_session_token_expires_ms = req_live_session_token(self)
+            self.oauth_init()
 
     def make_logger(self):
         self._logger = new_daily_rotating_file_handler('IbkrClient', os.path.join(var.LOGS_DIR, f'ibkr_client_{self.account_id}'))
 
-    def get_headers(
-            self,
-            request_method: str,
-            request_url: str
-    ):
+    def oauth_init(self):
+        from ibind.support.oauth import req_live_session_token
+        import signal
 
-        if (not self._use_oauth) or request_url == self.base_url + 'oauth/live_session_token':
+        # get live session token for OAuth authentication
+        self.live_session_token, self.live_session_token_expires_ms = req_live_session_token(self)
+
+        # start Tickler to maintain the connection alive
+        self._tickler = Tickler(self)
+        self._tickler.start()
+
+        # add signal handlers to gracefully shut down the Tickler and the client
+        existing_handler_int = signal.getsignal(signal.SIGINT)
+        existing_handler_term = signal.getsignal(signal.SIGTERM)
+
+        def _stop(signum, frame):
+            self.oauth_shutdown()
+
+            if signum == signal.SIGINT and callable(existing_handler_int):
+                existing_handler_int(signum, frame)
+
+            if signum == signal.SIGTERM and callable(existing_handler_term):
+                existing_handler_term(signum, frame)
+
+        signal.signal(signal.SIGINT, _stop)
+        signal.signal(signal.SIGTERM, _stop)
+
+    def oauth_shutdown(self):
+        if hasattr(self, '_tickler') and self._tickler is not None:
+            self._tickler.stop()
+
+        self.logout()
+
+    def get_headers(self, request_method: str, request_url: str):
+        if (not self._use_oauth) or request_url == f'{self.base_url}{var.IBIND_LIVE_SESSION_TOKEN_ENDPOINT}':
+            # No need for extra headers if we don't use oauth or getting live session token
             return {}
 
+        from ibind.support.oauth import generate_oauth_headers
         # get headers for endpoints other than live session token request
         headers = generate_oauth_headers(
             request_method=request_method,
